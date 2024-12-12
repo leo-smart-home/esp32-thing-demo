300e257e01bad65b3eb2a818047b9309d28d41b4
Leonid-98 <leonid.tsigrinski@gmail.com>
2024-12-12 17:56:31 +0200

    MQTT kinda works, no events called tho

--- src/app2/main.c
+++ src/app2/main.c
@@ -10,17 +10,73 @@
 #include "lwip/sys.h"          //system applications for light weight ip apps
 #include "driver/uart.h"
 
+// !===============================================================================================
+
+// #include "protocol_examples_common.h" //important for running different protocols in code
+#include "mqtt_client.h"       //provides important functions to connect with MQTT
+#include "esp_event.h"         //managing events of mqtt
+#include "nvs_flash.h"         //storing mqtt and wifi configs and settings
+#include "freertos/FreeRTOS.h" //it is important too if you want to run mqtt task independently and provides threads funtionality
+#include "freertos/task.h"     //MQTT communication often involves asynchronous operations, and FreeRTOS helps handle those tasks effectively
+#include "esp_log.h"           //log out put, do not use printf everywhere
+
+static void mqtt_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{                                                   // here esp_mqtt_event_handle_t is a struct which receieves struct event from mqtt app start funtion
+    if (event_id == MQTT_EVENT_CONNECTED)
+    {
+        printf("MQTT_EVENT_CONNECTED");
+        // esp_mqtt_client_subscribe(client, "your topic", 0);
+        printf("sent subscribe successful");
+    }
+    else if (event_id == MQTT_EVENT_DISCONNECTED)
+    {
+        printf("MQTT_EVENT_DISCONNECTED");
+    }
+    else if (event_id == MQTT_EVENT_SUBSCRIBED)
+    {
+        printf("MQTT_EVENT_SUBSCRIBED");
+    }
+    else if (event_id == MQTT_EVENT_UNSUBSCRIBED) 
+    {
+        printf("MQTT_EVENT_UNSUBSCRIBED");
+    }
+    else if (event_id == MQTT_EVENT_DATA)
+    {
+        printf("MQTT_EVENT_DATA");
+    }
+    else if (event_id == MQTT_EVENT_ERROR)
+    {
+        printf("MQTT_EVENT_ERROR");
+    }
+}
+
+static void mqtt_initialize(void)
+{ 
+    const esp_mqtt_client_config_t mqtt_cfg = {
+    };
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg); 
+
+    esp_err_t err;
+    err = esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, (esp_event_handler_t)mqtt_event_handler, client);
+    printf("esp_mqtt_client_register_event: %x\n", err);
+    err = esp_mqtt_client_set_uri(client, "mqtt://test.mosquitto.org:1883");
+    printf("esp_mqtt_client_set_uri: %x\n", err);
+    err = esp_mqtt_client_start(client);
+    printf("esp_mqtt_client_start: %x\n", err);
+    printf("MQTT Initialized\n");
+}
+
+// !===============================================================================================
 
 static void init_uart()
 {
-    uart_config_t uart_config = 
-    {
-        .baud_rate = 9600u,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
-    };
+    uart_config_t uart_config =
+        {
+            .baud_rate = 9600u,
+            .data_bits = UART_DATA_8_BITS,
+            .parity = UART_PARITY_DISABLE,
+            .stop_bits = UART_STOP_BITS_1,
+            .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
     uart_param_config(UART_NUM_0, &uart_config);
     uart_driver_install(UART_NUM_0, 1024, 0, 0, NULL, 0);
 }
@@ -30,8 +86,6 @@ int _write(int file, char *ptr_data, int len)
     (void)file; // Not used
     return uart_write_bytes(UART_NUM_0, (char *)ptr_data, (size_t)len);
 }
-
-
 
 const char *ssid = "STVWIFI_VBK9";
 const char *pass = "sTv572706";
@@ -98,5 +152,6 @@ void app_main(void)
     init_uart();
     nvs_flash_init();
     wifi_connection();
-    
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
+    mqtt_initialize();
 }
